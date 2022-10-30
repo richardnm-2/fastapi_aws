81f0adc6f12b4488362119b99855238859b03ade
Richard Nicolaas Meijerink <richardnicolaas@gmail.com>
2022-10-30 04:46:51 -0300

    lambda - locally OK / aws chrome crashes

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ def get_driver():
 
     options.binary_location = CHROME_BIN
 
+    '''
+    lambda: selenium.common.exceptions.WebDriverException: Message: unknown error: unable to discover open window in chrome
+    locally: works fine
+    '''
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-gpu")
@@ -28,6 +32,7 @@ def get_driver():
     options.add_argument(f"--disk-cache-dir={mkdtemp()}")
     options.add_argument("--remote-debugging-port=9222")
 
+    '''chrome crashes right from start on lambda, works fine locally'''
     # options.add_argument('--no-sandbox')
     # options.add_argument("--headless")
     # options.add_argument("--disable-dev-shm-usage")
